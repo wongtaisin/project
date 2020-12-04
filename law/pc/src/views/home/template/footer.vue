5a4bf026204eaa16bd1414918011d62a883451fd
wingddd <wongtaisin1024@gmail.com>
2020-12-04 09:53:21 +0800

    备案号

--- law/pc/src/views/home/template/footer.vue
+++ law/pc/src/views/home/template/footer.vue
@@ -3,7 +3,7 @@
     <div class="copy">
       {{data.sitename}} {{copyright}}
       <a
-        href="http://www.beian.miit.gov.cn"
+        href="https://beian.miit.gov.cn/"
         target="_blank"
       >{{data.code}}</a>
     </div>
@@ -15,29 +15,29 @@ export default {
   props: {
     data: {
       type: Object,
-      default () {
+      default() {
         return {}
       }
     }
   },
-  data () {
+  data() {
     return {
       copyright: '版权所有'
     }
   },
   computed: {
-    commonLanguage () {
+    commonLanguage() {
       return this.$store.getters['common/commonLanguage']
     }
   },
   watch: {
-    '$store.state.home.getLanguage' (data) {
+    '$store.state.home.getLanguage'(data) {
       if (data.code != 0) return false
       this.lang()
     }
   },
   methods: {
-    lang () {
+    lang() {
       if (this.commonLanguage == 'zh-HK') {
         this.copyright = '版權所有'
       } else if (this.commonLanguage == 'en-US') {
--- law/pc/vue.config.js
+++ law/pc/vue.config.js
@@ -1,38 +1,40 @@
 // process.env.NODE_ENV === 'development'; // 或简写 dev，意为开发环境
 // process.env.NODE_ENV === 'production'; // 或简写 prod，意为生产环境
-const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV) // 判断是否生产环境
+const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV); // 判断是否生产环境
 
 // 去除冗余的css
-const PurgecssPlugin = require('purgecss-webpack-plugin')
-const glob = require('glob-all')
-const path = require('path')
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const glob = require("glob-all");
+const path = require("path");
 
 //  开启 gzip 压缩
-const CompressionWebpackPlugin = require('compression-webpack-plugin')
-const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
 
 module.exports = {
   transpileDependencies: [], // 通过 Babel 显式转译一个依赖
   baseUrl: IS_PROD ? process.env.VUE_APP_SRC || "/webapp/pc/" : "./",
-  assetsDir: 'assets', // 静态资源目录
-  outputDir: process.env.outputDir || '../../../law/web/webapp/pc', // 输出文件目录
+  assetsDir: "assets", // 静态资源目录
+  outputDir: process.env.outputDir || "../../../law/web/webapp/pc", // 输出文件目录
   productionSourceMap: false, // 去除map文件
-  chainWebpack: config => { // 压缩图片
-    config.module
-      .rule('images')
-      .use('image-webpack-loader')
-      .loader('image-webpack-loader')
-      .options({
-        bypassOnDebug: true
-      })
-      .end()
-  },
+  // chainWebpack: config => { // 压缩图片
+  //   config.module
+  //     .rule('images')
+  //     .use('image-webpack-loader')
+  //     .loader('image-webpack-loader')
+  //     .options({
+  //       bypassOnDebug: true
+  //     })
+  //     .end()
+  // },
   devServer: {
     open: true, // 启动项目后自动开启浏览器
     port: 8010,
-    proxy: { // 跨域处理
-      '/api': { // 将 www.exaple.com 印射为/apis
-        target: 'http://118.89.64.135/', // 接口域名 测试http://129.204.194.44
+    proxy: {
+      // 跨域处理
+      "/api": {
+        // 将 www.exaple.com 印射为/apis
+        target: "http://118.89.64.135/", // 接口域名 测试http://129.204.194.44
         secure: false, // 如果是https接口，需要配置这个参数
         changeOrigin: true, // 是否跨域
         pathRewrite: {
@@ -40,7 +42,7 @@ module.exports = {
         }
       }
     }
-  },
+  }
   // webpack-dev-server 相关配置 谨慎使用（jdx失去分屏效果）
   // configureWebpack: config => {
   //   if (IS_PROD) {
@@ -67,4 +69,4 @@ module.exports = {
   //     ];
   //   }
   // }
-}
+};
