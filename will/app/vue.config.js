d2e157cbf87416b90ce4a5820ee403a23063ed94
wongtaisin <wongtaisin1024@gmail.com>
2021-04-04 20:19:40 +0800

    address

--- will/app/vue.config.js
+++ will/app/vue.config.js
@@ -1,39 +1,41 @@
 // process.env.NODE_ENV === 'development'; // 或简写 dev，意为开发环境
 // process.env.NODE_ENV === 'production'; // 或简写 prod，意为生产环境
-const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV) // 判断是否生产环境
+const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV); // 判断是否生产环境
 
 // 去除冗余的css
-const PurgecssPlugin = require('purgecss-webpack-plugin')
-const glob = require('glob-all')
-const path = require('path')
+// const PurgecssPlugin = require('purgecss-webpack-plugin')
+// const glob = require('glob-all')
+// const path = require('path')
 
-//  开启 gzip 压缩
-const CompressionWebpackPlugin = require('compression-webpack-plugin')
-const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
+// //  开启 gzip 压缩
+// const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
 
 module.exports = {
   transpileDependencies: [], // 通过 Babel 显式转译一个依赖
   baseUrl: IS_PROD ? process.env.VUE_APP_SRC || "/webapp/app/" : "./",
-  assetsDir: 'assets', // 静态资源目录
-  outputDir: process.env.outputDir || '../../../testament_bank/web/webapp/app', // 输出文件目录
+  assetsDir: "assets", // 静态资源目录
+  outputDir: process.env.outputDir || "../../../testament_bank/web/webapp/app", // 输出文件目录
   productionSourceMap: false, // 去除map文件
-  chainWebpack: config => { // 压缩图片
-    config.module
-      .rule('images')
-      .use('image-webpack-loader')
-      .loader('image-webpack-loader')
-      .options({
-        bypassOnDebug: true
-      })
-      .end()
-  },
+  // chainWebpack: config => { // 压缩图片
+  //   config.module
+  //     .rule('images')
+  //     .use('image-webpack-loader')
+  //     .loader('image-webpack-loader')
+  //     .options({
+  //       bypassOnDebug: true
+  //     })
+  //     .end()
+  // },
   devServer: {
     open: true, // 启动项目后自动开启浏览器
     port: 8021,
-    proxy: { // 跨域处理
-      '/api': { // 将 www.exaple.com 印射为/apis
+    proxy: {
+      // 跨域处理
+      "/api": {
+        // 将 www.exaple.com 印射为/apis
         // target: 'http://testament.hxz-dev.cn/', // 测试域名
-        target: 'http://www.szwills.org/', // 正式域名
+        target: "http://www.szwills.org/", // 正式域名
         secure: false, // 如果是https接口，需要配置这个参数
         changeOrigin: true, // 是否跨域
         pathRewrite: {
@@ -41,7 +43,7 @@ module.exports = {
         }
       }
     }
-  },
+  }
   // webpack-dev-server 相关配置 谨慎使用（失去分屏效果）
   // configureWebpack: config => {
   //   if (IS_PROD) {
@@ -68,4 +70,4 @@ module.exports = {
   //     ];
   //   }
   // }
-}
+};
