0f72205db58771ed05c497778d302b4c9e230d6d
wingddd <wongtaisin1024@gmail.com>
2026-01-05 17:28:12 +0800

    修复样式

--- app-react19/vite.config.ts
+++ app-react19/vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: wingddd wongtaisin1024@gmail.com
  * @Date: 2026-01-05 11:08:08
  * @LastEditors: wingddd wongtaisin1024@gmail.com
- * @LastEditTime: 2026-01-05 17:15:17
+ * @LastEditTime: 2026-01-05 17:17:55
  * @FilePath: \project\app-react19\vite.config.ts
  * @Description:
  *
@@ -10,6 +10,7 @@
  */
 // import react from '@vitejs/plugin-react' // 开启react-compiler
 import { defineConfig } from 'vite'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js' // TODO: qiankun 将 CSS 注入 JS，避免因路径错误导致样式丢失
 import qiankun from 'vite-plugin-qiankun'
 
 // https://vite.dev/config/
@@ -31,7 +32,8 @@ export default defineConfig(({ mode }) => {
 			// 		plugins: [['babel-plugin-react-compiler']]
 			// 	}
 			// }), // TODO: 关闭之后，可dev模式下，react-compiler不生效，quankun生效
-			qiankun('app-react19', { useDevMode: true })
+			qiankun('app-react19', { useDevMode: true }),
+			cssInjectedByJsPlugin()
 		],
 
 		// 开启react-compiler
