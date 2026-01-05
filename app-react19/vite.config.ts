8df8cc863676a8ec5f323e26900822f9e5fa7902
wingddd <wongtaisin1024@gmail.com>
2026-01-05 17:16:33 +0800

    关闭 plugins 删除 react()，dev模式下quankun生效

--- app-react19/vite.config.ts
+++ app-react19/vite.config.ts
@@ -2,17 +2,15 @@
  * @Author: wingddd wongtaisin1024@gmail.com
  * @Date: 2026-01-05 11:08:08
  * @LastEditors: wingddd wongtaisin1024@gmail.com
- * @LastEditTime: 2026-01-05 16:36:26
+ * @LastEditTime: 2026-01-05 17:15:17
  * @FilePath: \project\app-react19\vite.config.ts
  * @Description:
  *
  * Copyright (c) 2026 by wongtaisin1024@gmail.com, All Rights Reserved.
  */
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react' // 开启react-compiler
 import { defineConfig } from 'vite'
 import qiankun from 'vite-plugin-qiankun'
-
-// TODO: react项目，需要build，然后运行生产preview模式下本地才可以正常运行（Vite 开发模式下输出的是 ES Module，qiankun 加载的是 CommonJS 模块，会报错）
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -23,18 +21,25 @@ export default defineConfig(({ mode }) => {
 
 	const currentEnv = mode
 
-	console.log('currentEnv', currentEnv, urls[currentEnv])
+	console.log('当前环境：', currentEnv, urls[currentEnv])
 
 	return {
 		base: urls[currentEnv],
 		plugins: [
-			react({
-				babel: {
-					plugins: [['babel-plugin-react-compiler']]
-				}
-			}),
+			// react({
+			// 	babel: {
+			// 		plugins: [['babel-plugin-react-compiler']]
+			// 	}
+			// }), // TODO: 关闭之后，可dev模式下，react-compiler不生效，quankun生效
 			qiankun('app-react19', { useDevMode: true })
 		],
+
+		// 开启react-compiler
+		esbuild: {
+			jsx: 'automatic',
+			jsxImportSource: 'react'
+		},
+
 		server: {
 			port: 7003,
 			origin: 'http://localhost:7003',
