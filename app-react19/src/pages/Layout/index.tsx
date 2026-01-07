5f2e47ff6d92b94f48aedd6064fa6d505d8b301f
wingddd <wongtaisin1024@gmail.com>
2026-01-07 08:30:19 +0800

    react19 production 环境下，打开 react-compiler

--- app-react19/src/pages/Layout/index.tsx
+++ app-react19/src/pages/Layout/index.tsx
@@ -2,8 +2,8 @@
  * @Author: wingddd wongtaisin1024@gmail.com
  * @Date: 2025-12-29 15:51:44
  * @LastEditors: wingddd wongtaisin1024@gmail.com
- * @LastEditTime: 2026-01-05 11:53:56
- * @FilePath: \app-react19\src\pages\Layout\index.tsx
+ * @LastEditTime: 2026-01-07 08:28:02
+ * @FilePath: \project\app-react19\src\pages\Layout\index.tsx
  * @Description:
  *
  * Copyright (c) 2025 by wongtaisin1024@gmail.com, All Rights Reserved.
@@ -18,9 +18,9 @@ const Layout = () => {
   return (
     <div>
       {loading === true ? (
-        <div> react-router的页面加载中...</div>
+        <div> react-router19的页面加载中...</div>
       ) : (
-        <Suspense fallback={<div>Suspense的页面加载中...</div>}>
+        <Suspense fallback={<div>子应用的页面加载中...</div>}>
           <Outlet />
         </Suspense>
       )}
--- app-react19/vite.config.ts
+++ app-react19/vite.config.ts
@@ -2,52 +2,56 @@
  * @Author: wingddd wongtaisin1024@gmail.com
  * @Date: 2026-01-05 11:08:08
  * @LastEditors: wingddd wongtaisin1024@gmail.com
- * @LastEditTime: 2026-01-05 17:17:55
+ * @LastEditTime: 2026-01-07 08:26:06
  * @FilePath: \project\app-react19\vite.config.ts
  * @Description:
  *
  * Copyright (c) 2026 by wongtaisin1024@gmail.com, All Rights Reserved.
  */
-// import react from '@vitejs/plugin-react' // 开启react-compiler
+import react from '@vitejs/plugin-react' // 开启react-compiler
 import { defineConfig } from 'vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js' // TODO: qiankun 将 CSS 注入 JS，避免因路径错误导致样式丢失
 import qiankun from 'vite-plugin-qiankun'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-	const urls = {
-		production: 'http://localhost:7003/',
-		development: '/'
-	} as any
+  const urls = {
+    production: 'http://localhost:7003/',
+    development: '/'
+  } as any
 
-	const currentEnv = mode
+  const currentEnv = mode
 
-	console.log('当前环境：', currentEnv, urls[currentEnv])
+  console.log('当前环境：', currentEnv, urls[currentEnv])
 
-	return {
-		base: urls[currentEnv],
-		plugins: [
-			// react({
-			// 	babel: {
-			// 		plugins: [['babel-plugin-react-compiler']]
-			// 	}
-			// }), // TODO: 关闭之后，可dev模式下，react-compiler不生效，quankun生效
-			qiankun('app-react19', { useDevMode: true }),
-			cssInjectedByJsPlugin()
-		],
+  const isProd = mode === 'production'
 
-		// 开启react-compiler
-		esbuild: {
-			jsx: 'automatic',
-			jsxImportSource: 'react'
-		},
+  return {
+    base: urls[currentEnv],
+    plugins: [
+      isProd &&
+        react({
+          babel: {
+            plugins: [['babel-plugin-react-compiler']]
+          }
+        }), // TODO: 关闭之后，可dev模式下，react-compiler不生效，quankun生效
+      qiankun('app-react19', { useDevMode: true }),
+      cssInjectedByJsPlugin()
+    ],
 
-		server: {
-			port: 7003,
-			origin: 'http://localhost:7003',
-			headers: {
-				'Access-Control-Allow-Origin': '*'
-			}
-		}
-	}
+    // 开启react-compiler
+    esbuild: {
+      jsx: 'automatic',
+      jsxImportSource: 'react'
+    },
+
+    server: {
+      port: 7003,
+      origin: 'http://localhost:7003',
+      cors: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    }
+  }
 })
